46d5f1227375be44bb03d5d220c485384fcecaae
Diego Alvarez <owl@Diegos-MacBook-Pro.local>
2022-06-18 17:37:35 -0300

    final optimizations

---
+++ critical.js
@@ -0,0 +1,23 @@
+var request = require('request');
+var path = require( 'path' );
+var criticalcss = require("criticalcss");
+var fs = require('fs');
+var tmpDir = require('os').tmpdir();
+
+var cssUrl = 'https://cdn.shopify.com/s/files/1/0507/6097/4513/t/18/assets/theme.scss.css';
+var cssPath = path.join( tmpDir, 'style.css' );
+request(cssUrl).pipe(fs.createWriteStream(cssPath)).on('close', function() {
+  criticalcss.getRules(cssPath, function(err, output) {
+    if (err) {
+      throw new Error(err);
+    } else {
+      criticalcss.findCritical("https://pearlofmyheart.eu/\?preview_theme_id\=133576163562", { rules: JSON.parse(output) }, function(err, output) {
+        if (err) {
+          throw new Error(err);
+        } else {
+          console.log(output);
+        }
+      });
+    }
+  });
+});
